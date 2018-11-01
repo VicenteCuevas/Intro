6ff61ee22020a82426647ae4129d759e6c8d249d
VicenteCuevas <43301814+VicenteCuevas@users.noreply.github.com>
2018-11-01 17:20:57 -0300

    Delete app.firebase.config.ts

--- src/app/app.firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const FIREBASE_CONFIG = {
-	apiKey: "AIzaSyCJ-4trVQNnC7POnQHq-WiM2skbEJYf2KA",
-    authDomain: "test-project-ec2e2.firebaseapp.com",
-    databaseURL: "https://test-project-ec2e2.firebaseio.com",
-    projectId: "test-project-ec2e2",
-    storageBucket: "test-project-ec2e2.appspot.com",
-    messagingSenderId: "719028707475"
-}
